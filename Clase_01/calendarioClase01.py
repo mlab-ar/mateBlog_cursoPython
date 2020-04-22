99079de4ab259182a26469f9601783428898ea21
Juan P. Romano <35672464+jpromanonet@users.noreply.github.com>
2020-04-22 17:59:17 -0300

    Update calendarioClase01.py

--- Clase_01/calendarioClase01.py
+++ Clase_01/calendarioClase01.py
@@ -1,6 +1,8 @@
 # Vamos a importar
 # la libreria que imprime el calendario asi:
 import calendar
+# y la que frena la ejecución asi:
+import time
 
 # Ahora vamos a pedirle al usuario
 # que ingrese el año que quiere ver
@@ -13,3 +15,5 @@ month = int(input("¿Y que mes? "))
 # Para finalizar imprimimos
 # el mes del año socilitado
 print(calendar.month(year, month))
+# Paramos la ejecución para que no se cierre
+time.sleep(10000)
